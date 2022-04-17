f366d67865c9750765aa88b5934925e36497c1be
Nate <schmid.n@gmail.com>
2022-04-16 21:24:29 -0600

    Adding an interface for DB communications
    This PR resolves #3 by adding a golang interface that defines DB
    communication functions.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NateJSchmidt/chronicler
+
+go 1.18
---
+++ internal/pkg/db/db_interface.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"time"
+)
+
+// DBInterface is the interface that wraps the functions chronicler uses to communicate with the database.
+type DBInterface interface {
+	// Write a single event to a given stream.
+	WriteEvent(streamName string, event string, version string) error
+
+	// Conditionally write an event to a given stream.  If the current last event in the stream is at previousPosition,
+	// then the write will succeed; otherwise, it will fail and an error will be returned.
+	ConditionallyWriteEvent(streamName string, event string, version string, previousPosition uint64) error
+
+	// Read a single event from a given stream at a given position.
+	ReadEvent(streamName string, position uint64) (string, error)
+
+	// Reads all of the events from a given stream.
+	ReadAllEvents(streamName string) (string, error)
+
+	// Reads all of the events from a given stream, starting at a given position.
+	ReadEventsFromPosition(streamName string, position uint64) (string, error)
+
+	// Reads all of the events from a given stream, starting at a given datetime.
+	ReadEventsFromTime(streamName string, datetime time.Time) (string, error)
+}
