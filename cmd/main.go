09ba70ddce97f94856a951cad6226be8f47453b8
Nate <schmid.n@gmail.com>
2022-04-30 07:41:54 -0600

    Add database libraries and examples
    This resolves #5.  We pull in the database/sql library, which will be the API that we interact with the database.  The postgres driver is pq, which appears to be the most used PostgreSQL driver in golang.  We may need to find a library that does connection pooling, but this can be for later since there may be difficulties based on our database partitioning design.

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
-func main() {
-	r := gin.Default()
+// configureRoutes sets up the routes for the REST server
+func configureRoutes(r *gin.Engine) {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -14,5 +18,52 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello World")
 	})
+}
+
+// createDatabase creates a database named dbName
+func createDatabase(dbName string) {
+	db, err := sql.Open("postgres", "postgres://postgres:password1234@localhost:5432/postgres?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create the database
+	_, err = db.Exec("CREATE DATABASE " + dbName)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// createTables creates the tables in the database specified by dbName
+func createTables(dbName string) {
+	db, err := sql.Open("postgres", "postgres://postgres:password1234@localhost:5432/"+dbName+"?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create the table
+	sqlStatement := `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT NOT NULL, email TEXT NOT NULL);`
+	_, err = db.Exec(sqlStatement)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// main entrypoint for the program
+func main() {
+	// create necessary resources
+	r := gin.Default()
+	dbName := "testdb"
+
+	// perform configuration for REST server
+	configureRoutes(r)
+
+	// perform configuration for database
+	createDatabase(dbName)
+	createTables(dbName)
+
+	// start the server
 	r.Run()
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/NateJSchmidt/chronicler
 
 go 1.18
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/lib/pq v1.10.5
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
